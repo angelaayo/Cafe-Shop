bb2bfbd9a6a40bf323e9da2187de1d6a409285ab
Angela Ayodeji <angelaayodeji@gmail.com>
2026-06-11 00:14:35 -0500

    Initial commit

---
+++ src/tests/App.test.jsx
@@ -0,0 +1,21 @@
+// App.test.jsx
+
+import { describe, expect, test } from "vitest";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import App from "../App";
+
+describe("App Component", () => {
+  test("Render Magnificent Monkeys", () => {
+    const container = render(<App />);
+    expect(container).toMatchSnapshot();
+  });
+  test("Renders radical rhinos after button click", async () => {
+    const user = userEvent.setup();
+
+    render(<App />);
+    const button = screen.getByRole("button", { name: "Click Me" });
+    await user.click(button);
+    expect(screen.getByRole("heading").textContent).toMatch(/radical rhinos/i);
+  });
+});
